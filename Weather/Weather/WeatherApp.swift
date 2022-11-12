4384399acabade1a49404594828ff40ef0d7969f
Aditya Mishra <31aditya0193@gmail.com>
2022-11-12 20:05:02 +0530

    Initial Commit
    - Created iOS App in repo.

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Aditya Mishra on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
